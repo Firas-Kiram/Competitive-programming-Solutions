48a0e37d002b42dec0d3f5a876f1b3288e0d7398
Corvus <135715029+Firas-Kiram@users.noreply.github.com>
2023-10-20 13:00:51 +0100

    Create Equation.cpp

---
+++ Binary Search/Equation.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long long int lli;
+typedef long double ld;
+#define MP make_pair
+#define pb push_back
+#define pi pair<ll,ll>
+#define F first
+#define S second
+#define OUTRET(...) { OUT(__VA_ARGS__); return; }
+#define OUT(...) cout << __VA_ARGS__ << '\n'
+#define all(x) (x).begin(), (x).end()
+#define yes cout<<"YES"<<ln;
+#define no cout<<"NO"<<ln;
+#define sz(v) ((int)((v).size()))
+#define ln '\n'
+const ll INF = 1000000000;
+const ll MOD = 1073741824;
+const ll M = 1e6 + 8432;
+const int N= 5e5+10;
+const ll EPS = 1e-5;
+ld c;
+
+bool good(ld x){
+    return (x*x)+sqrt(x) >= c;
+}
+
+void solve(){
+    cin >> c;
+    ld l = 0.0,r = 1;
+    while(!good(r)) r*=2;
+    int itr = 300;
+    while(l<=r&&itr--){
+        ld md = (l+r)/2;
+        if(good(md)){
+            r = md;
+        }else{
+            l = md;
+        }
+    }
+
+
+    cout << fixed << setprecision(15) << r << ln;
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("planting.in", "r", stdin);
+    //freopen("planting.out", "w", stdout);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+ 
+    return 0;
+}
