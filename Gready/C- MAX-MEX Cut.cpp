6616366d526a276704c340d64907c6f68e77fdcb
Corvus <135715029+Firas-Kiram@users.noreply.github.com>
2024-06-09 22:30:14 +0100

    Create C- MAX-MEX Cut.cpp

---
+++ Gready/C- MAX-MEX Cut.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long long int lli;
+typedef long double ld;
+#define MP make_pair
+#define pb push_back
+#define pi pair<ll,ll>
+#define F first
+#define S second
+#define OUTRET(...) { OUT(__VA_ARGS__); return; }
+#define OUT(...) cout << __VA_ARGS__ << '\n'
+#define dbg(x) cout <<#x<<": "<<x<< ln;
+#define all(x) (x).begin(), (x).end()
+#define yes cout<<"YES"<<ln;
+#define no cout<<"NO"<<ln;
+#define sz(v) ((int)((v).size()))
+#define ln '\n'
+#define PI 3.1415926535897932384626433832795
+const ll INF = 1000000000;
+const ll MOD = 1000000007;
+const ll M = 1e6 + 8432;
+const ll N = 1000000000000;
+
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+
+template<class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+template<class T>
+bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+
+void io() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+ 
+ll get(char c) {return c - '0';}
+
+void solve(){
+	ll n;cin >> n;
+	string a , b;cin >> a >> b;
+	ll ans = 0 , one = 0 , tot = 0;;
+	vector <pair <char , int>> s;
+	for(int i = 0 ; i < n ; i += 1){
+		ll x = get(a[i]) + get(b[i]);
+		if(x == 1) ans += 2;
+		else {
+			s.pb({a[i] , i});
+			one += (a[i] == '1');
+		}
+	}
+	vector <bool> vis(n + 500 , false);
+	for(int i = 0 ; i < sz(s) ;i += 1){
+		if(s[i].F == '1'){
+			if(i){
+				if(s[i - 1].S == s[i].S - 1 && s[i - 1].F == '0' && !vis[s[i - 1].S]){
+					vis[s[i - 1].S] = 1;
+					tot += 1;
+					continue;
+				}
+			}
+			if(i + 1 < sz(s) && s[i + 1].S == s[i].S + 1 && s[i + 1].F == '0' && !vis[s[i + 1].S]){
+				vis[s[i + 1].S] = 1;
+				tot += 1;
+			}
+		}
+	}
+	cout << ' ' << ans + (sz(s) - one - tot) + tot*2 << ln;
+}	
+
+
+int main() {
+    io();
+    int T = 1;
+    #ifndef ONLINE_JUDGE
+		freopen("input.in", "r", stdin);
+		freopen("output.out", "w", stdout);
+	#endif // ONLINE_JUDGE
+	cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
