193136d4e7dbbfe89126b7a0a13177be0702569b
Corvus <135715029+Firas-Kiram@users.noreply.github.com>
2024-07-02 11:31:13 +0100

    Create B-Streamer night.cpp

---
+++ Dynamic Programming/B-Streamer night.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define MP make_pair
+#define pb push_back
+#define pi pair<ll,ll>
+#define F first
+#define S second
+#define OUTRET(...) { OUT(__VA_ARGS__); return; }
+#define OUT(...) cout << __VA_ARGS__ << '\n'
+#define dbg(x) cout <<#x<<": "<<x<< ln;
+#define all(x) (x).begin(), (x).end()
+#define yes cout<<"YES"<<ln;
+#define no cout<<"NO"<<ln;
+#define sz(v) ((int)((v).size()))
+#define ln '\n'
+const ll INF = 1000000000;
+const ll MOD = 1e9 + 7;
+const ll M = 1e6 + 8432;
+const ll N = 2e5 + 2000;
+ 
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+ 
+template<class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+template<class T>
+bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+ 
+void io() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+vector <ll> v[N];
+ll dp[N];
+ll n,k;
+
+ll work(ll i){
+    if(i == n + 1) return 0;
+    ll&ret = dp[i];
+    if(ret != -1) return ret;
+    ret = work(i + 1);
+    for(auto it : v[i]) ret = max(ret , work(it) + 1);
+    return ret;
+}
+
+void solve() {
+    cin >> n >> k;
+    for(int i = 0 ; i < k ; i ++){
+        ll l , r;cin >> l >> r;
+        v[l].pb(r);
+    }
+    memset(dp , -1 , sizeof(dp));
+    cout << work(0) << ln;
+}
+ 
+int main() {
+    io();
+    #ifndef ONLINE_JUDGE
+        freopen("input.in", "r", stdin);
+        freopen("output.out", "w", stdout);
+    #endif // ONLINE_JUDGE
+    int T = 1; 
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
