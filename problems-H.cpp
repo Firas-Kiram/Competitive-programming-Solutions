eb02919d3230111df35325540fd170852333933d
Corvus <135715029+Firas-Kiram@users.noreply.github.com>
2024-07-13 14:52:28 +0100

    Create problems-H.cpp

---
+++ problems-H.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define MP make_pair
+#define pb push_back
+#define pi pair<ll,ll>
+#define F first
+#define S second
+#define OUTRET(...) { OUT(__VA_ARGS__); return; }
+#define OUT(...) cout << __VA_ARGS__ << '\n'
+#define dbg(x) cout <<#x<<": "<<x<< ln;
+#define all(x) (x).begin(), (x).end()
+#define yes cout<<"YES"<<ln;
+#define no cout<<"NO"<<ln;
+#define sz(v) ((int)((v).size()))
+#define ln '\n'
+const ll INF = 1000000000;
+const ll MOD = 1e9 + 7;
+const ll M = 1e6 + 8432;
+const ll N = 2e5 + 2000;
+ 
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+ 
+template<class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+template<class T>
+bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+ 
+void io() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+
+void solve() {
+    ll n,p , nx = 0 , k = 0 , ans = 0;
+    cin >> n >> p;
+    ll m = p;
+    while(p){
+        if(nx + p > n - 1) break;
+        nx += p;
+        k ++;
+        p --;
+    }
+    /*
+        idea is to get the number of steps you can do in n
+        and start in point with condition the last step should be the end
+        to get the max sum of numbers
+    */
+    ll x = m - k + 1;
+    p = n;
+    while(n - x > 0 && k--){
+        p -= x;
+        ans += p;
+        x += 1;
+    }
+    cout << ans + n << ln;
+}
+
+int main() {
+    io();
+    #ifndef ONLINE_JUDGE
+        freopen("input.in", "r", stdin);
+        freopen("output.out", "w", stdout);
+    #endif // ONLINE_JUDGE
+    int T = 1; 
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
