f4e8f0407a9ed653911b148381b244c77ecd544d
Corvus <135715029+Firas-Kiram@users.noreply.github.com>
2024-08-20 13:44:16 +0100

    Create F- Expected Median.cpp

---
+++ Math/Combinatorics/F- Expected Median.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define pb push_back
+#define pi pair<ll,ll>
+#define F first
+#define S second
+#define ln '\n'
+#define dbg(x) cout <<#x<<": "<<x<< ln;
+void watch(vector<ll> v){for (auto x : v)cout<<x<<" ";cout <<ln;}
+#define all(x) (x).begin(), (x).end()
+#define yes cout<<"YES"<<ln;
+#define no cout<<"NO"<<ln;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+const ll MOD = 1e9 + 7;
+
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+template<class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+template<class T>
+bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; } 
+void io() {ios::sync_with_stdio(false);cin.tie(NULL);}
+int dx[]{1, -1, 0, 0, 1, 1, -1, -1};
+int dy[]{0, 0, 1, -1, 1, -1, 1, -1};
+const ll N = 1e5 + 10;
+
+template <typename T, typename U>
+int mul(T u, U v, int mod = MOD) {
+    return ((u % mod) * 1ll * (v % mod)) % mod;
+}
+template <typename T, typename U>
+int add(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll + (v % mod) + mod) % mod;
+}
+template <typename T, typename U>
+int sub(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll - (v % mod) + mod) % mod;
+}
+const ll MAXN = 1e6 + 100;
+ll fac[MAXN + 1];
+ll inv[MAXN + 1];
+
+ll exp(ll x, ll n, ll m) {
+    x %= m;
+    ll res = 1;
+    while (n > 0) {
+        if (n % 2 == 1) { res = res * x % m; }
+        x = x * x % m;
+        n /= 2;
+    }
+    return res;
+}
+
+void factorial() {
+    fac[0] = 1;
+    for (int i = 1; i <= MAXN; i++) { fac[i] = fac[i - 1] * i % MOD; }
+}
+
+void inverses() {
+    inv[MAXN] = exp(fac[MAXN], MOD - 2, MOD);
+    for (int i = MAXN; i >= 1; i--) { inv[i - 1] = inv[i] * i % MOD; }
+}
+
+ll choose(int n, int r) { return fac[n] * inv[r] % MOD * inv[n - r] % MOD; }
+
+void solve() {
+    ll n , k;
+    cin >> n >> k;
+    vector <ll> v(n) , a(n);
+    for(int i = 0 ; i < n ; i ++)
+        cin >> v[i]; 
+    sort(all(v));
+    ll p = k / 2;
+    for(int i = 0 ; i < n ; i ++ ){
+        a[i] = mul(choose(i , p) , choose(n - i - 1, p));
+    }
+    ll res = 0;
+    for(int i = p ; i < n - p ; i ++){
+        res += mul(a[i] , v[i]);
+    }
+    res %= MOD;
+    cout << res << ln;
+}
+
+int main() {
+    io();
+    factorial();
+    inverses();
+    #ifndef ONLINE_JUDGE
+        freopen("input.in", "r", stdin);
+        freopen("output.out", "w", stdout);
+    #endif // ONLINE_JUDGE
+    int T = 1; 
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
