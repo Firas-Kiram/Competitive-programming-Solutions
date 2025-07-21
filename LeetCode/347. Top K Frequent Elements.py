c300e361f1390e33439b028f1f8765bef6ca4e6b
Firas Mohamed El Amine Kiram <135715029+Firas-Kiram@users.noreply.github.com>
2025-07-21 16:17:34 +0100

    Create 347. Top K Frequent Elements.py

---
+++ LeetCode/347. Top K Frequent Elements.py
@@ -0,0 +1,15 @@
+# problem : Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/
+from heapq import heappush , heappop 
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        a = [0] * (20001)
+
+        for x in nums :
+            a[x + 10000] += 1
+        heap = []
+        for i in range(20001):
+            heappush(heap , (a[i] , i - 10000))
+            if len(heap) > k : heappop(heap)
+
+        return [x[1] for x in heap]
+        
